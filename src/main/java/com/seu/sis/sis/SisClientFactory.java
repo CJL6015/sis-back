69f991e39f8b7cbf1fee5ef2ef6abe1d1b5636cc
Jiale <cjl601523@gmail.com>
2023-10-28 10:41:07 +0800

    feat: 新增测点数据同步功能

--- src/main/java/com/seu/sis/sis/SisClientFactory.java
+++ src/main/java/com/seu/sis/sis/SisClientFactory.java
@@ -1,12 +1,14 @@
 package com.seu.sis.sis;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author 陈小黑
  */
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class SisClientFactory {
@@ -20,6 +22,7 @@ public class SisClientFactory {
             sisClient.createConnection(config.getIp(), config.getPort(),
                     config.getPath(), config.getUser(), config.getPassword());
         } catch (Exception e) {
+            log.error("sis实例化异常", e);
             sisClient = null;
         }
         return sisClient;
--- src/main/java/com/seu/sis/task/PointDataTask.java
+++ src/main/java/com/seu/sis/task/PointDataTask.java
@@ -1,11 +1,11 @@
 package com.seu.sis.task;
 
 import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.io.FileUtil;
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.WriteApi;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
-import com.seu.sis.dao.domain.PointCfg;
 import com.seu.sis.dao.service.PointCfgService;
 import com.seu.sis.influx.InfluxConfig;
 import com.seu.sis.sis.SisClient;
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,6 +32,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PointDataTask {
     private static final String VALUE_KEY = "value";
+    private static final String TXT_PATH = "E:\\point\\point.txt";
 
 
     private final SisClient sisClient;
@@ -49,8 +51,12 @@ public class PointDataTask {
     @Scheduled(fixedRate = 10000)
     public void fetchAndStorePointData() {
         try {
-            List<PointCfg> list = pointCfgService.list();
-            List<String> names = list.stream().map(PointCfg::getPointId).collect(Collectors.toList());
+//            List<PointCfg> list = pointCfgService.list();
+//            List<String> names = list.stream().map(PointCfg::getPointId).collect(Collectors.toList());
+            String s = FileUtil.readUtf8String(TXT_PATH);
+            String[] before = s.split(",");
+            List<String> names = Arrays.asList(before);
+            log.info("本次同步点号:{}", names);
             if (CollectionUtil.isEmpty(names)) {
                 log.info("数据库测点为空");
                 return;
@@ -61,13 +67,23 @@ public class PointDataTask {
                 log.error("sis读取时实值异常");
                 return;
             }
+            log.info("sis读取结果:{}", realDataByNames);
             List<Point> points = new ArrayList<>();
+            List<String> exits = new ArrayList<>();
             realDataByNames.forEach((key, value) -> {
                 Point point = Point.measurement(key)
                         .addField(VALUE_KEY, value.getValue())
                         .time(Instant.now(), WritePrecision.NS);
                 points.add(point);
+                exits.add(key);
             });
+
+            if (names.size() != exits.size()) {
+                List<String> remain = Arrays.stream(before)
+                        .filter(e -> !exits.contains(e))
+                        .collect(Collectors.toList());
+                log.info("{}测点未获取到值", remain);
+            }
             WriteApi writeApi = influxDBClient.getWriteApi();
             writeApi.writePoints(config.getBucket(), config.getOrg(), points);
             log.info("从SIS同步到InfluxDB结束,共{}测点", points.size());
